84047a153659f88c67ab76d35bf131fa07dfb9b8
matteofacci <matteofacci.1994@gmail.com>
2021-05-13 21:18:23 +0200

    Minor update

--- mars surface/meshCreation.m
+++ mars surface/meshCreation.m
@@ -1,25 +1,44 @@
-% https://www.uahirise.org/ESP_059329_2095
+% https://www.uahirise.org/ESP_068360_1985 (25 February 2021)
 
 clear; close all; clc;
 
 %% Set the parameters depending on the picture
 
-%I = imread('ESP_068360_1985.jpg'); % 29.4 cm/pixel (Perseverance)
-%I = imread('ESP_059329_2095_RGB.jpg'); % 29.4 cm/pixel
-%I = imread('mars_surface.jpg'); % 504.38 m/pixel
-%I = imread('mars_vulcanic.jpg'); % 504.38 m/pixel
-I = imread('volcans-mars-hirise.jpg'); % 5.196 m/pixel
+choice = 1;
 
-%mpp = 0.294; % meters per pixel (ESP pics)
-%mpp = 0.50438; % meters per pixel (mars surface pic, fake param)
-mpp = 5.196/10; % meters per pixel (volcans pic)
+switch choice
+    case 1
+        I = imread('ESP_068360_1985.jpg'); % 29.5 cm/pixel (Perseverance)
+        mpp = 0.295; % meters per pixel (ESP pics)
+        % gaussian filter smoothing factor (sigma = 16 for maximum smoothing)
+        sigma = 16; %(ESP pic)
+        maxHeigth = 20; % (fake param)
+        
+        
+    case 2
+        I = imread('ESP_059329_2095_RGB.jpg'); % 29.5 cm/pixel
+        mpp = 0.295; % meters per pixel (ESP pics)
+        % gaussian filter smoothing factor (sigma = 16 for maximum smoothing)
+        sigma = 16; %(ESP pic)
+        maxHeigth = 20; % (fake param)
+        
+        
+    case 3
+        I = imread('mars_surface.jpg'); % 504.38 m/pixel
+        mpp = 0.50438; % meters per pixel (mars surface pic, fake param)
+        % gaussian filter smoothing factor (sigma = 16 for maximum smoothing)
+        sigma = 16; %(ESP pic)
+        maxHeigth = 50; % (fake param)
+        
+    case 4
+        I = imread('volcans-mars-hirise.jpg'); % 5.196 m/pixel
+        mpp = 5.196/10; % meters per pixel (volcans pic)
+        % gaussian filter smoothing factor (sigma = 16 for maximum smoothing)
+        sigma = 4; %(volcans pic)
+        maxHeigth = 400/10; % (volcanos pic h_real = 400 m)
+end
 
-% gaussian filter smoothing factor (sigma = 16 for maximum smoothing)
 
-%sigma = 16; %(ESP pic)
-sigma = 4; %(volcans pic)
-
-maxHeigth = 400/10; % (volcanos pic h_real = 400 m)
 
 %% Plots
 
@@ -32,7 +51,7 @@ RI.XWorldLimits = [0 xLimit];
 RI.YWorldLimits = [0 yLimit];
 imshow(I)
 hax = gca;
-hax.YTickLabel = flipud(hax.YTickLabel); % flipping the y-axis 
+hax.YTickLabel = flipud(hax.YTickLabel); % flipping the y-axis
 movegui('west');
 
 roi = drawline('Color','r'); % draw line interactively
@@ -42,7 +61,7 @@ startPose = [roi.Position(1,2) roi.Position(1,1)];
 goalPose = [roi.Position(2,2) roi.Position(2,1)];
 
 I = flip(I ,1);           % vertical flip
-Iblur = imgaussfilt(I,sigma); % gaussian filter 
+Iblur = imgaussfilt(I,sigma); % gaussian filter
 
 grayScale = rgb2gray(Iblur);   % Grayscale Image
 
@@ -78,7 +97,7 @@ pixvals = improfile(grayScale,startPose,goalPose);
 terrainProfile(1,:) = (1:length(pixvals))*mpp;
 terrainProfile(2,:) = pixvals;
 
-figure(3) 
+figure(3)
 title('Terrain Profile')
 %plot(1:length(pixvals),pixvals,'k'), axis tight, title('pixel values')
 plot(terrainProfile(1,:),terrainProfile(2,:),'k');
