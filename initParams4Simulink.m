a83d859b7e9064b2de806ae449e5a02cdc1b853a
matteofacci <matteofacci.1994@gmail.com>
2021-10-29 00:28:32 +0200

    Minor updates

--- initParams4Simulink.m
+++ initParams4Simulink.m
@@ -6,7 +6,7 @@ sampleTime = 0.5; % [s]
 % constant data
 m = 1025;
 g = -9.807;
-r = 0.25;
+r = 1.25;
 l1 = 2;
 lB = 1;
 l2 = 1;
@@ -63,7 +63,40 @@ timeStartPiece(end+1) = totalTime;
 
 terrainProfileTime = [timeStartPiece;slope]';
 
-%% Contact Points and Critical Points Positions
+%% Contact Points
+
+% ALERT : if the wheel has two contact points they have been added to the
+% array, the right one (new ramp) is chosen, the left one (actual ramp)
+% discarded but memorized as third column in the vector
+
+CP(1,1) = terrainProfileTime(1,1);
+CP(1,2) = terrainProfileTime(1,2);
+CP(1,3) = NaN;
+
+for i = 2:length(terrainProfileTime)-1
+
+    CP(i,1) = terrainProfileTime(i,1);
+
+    if alpha(i) > alpha(i-1)
+        [xCP_left,zCP_left,xCP_right,zCP_right] =...
+            tan2circle(terrainProfileTime(i-1,1),terrainProfileTime(i-1,2),...
+            terrainProfileTime(i,1),terrainProfileTime(i,2),...
+            terrainProfileTime(i+1,1),terrainProfileTime(i+1,2),alpha(i-1),alpha(i),r);
+        CP(i,1) = xCP_right;
+        CP(i,2) = zCP_right;
+        CP(i,3) = zCP_left;
+
+    else
+        CP(i,2) = terrainProfileTime(i,2);
+        CP(i,3) = NaN;
+    end
+end
+
+CP(end,1) = terrainProfileTime(end,1);
+CP(end,2) = terrainProfileTime(end,2);
+CP(end,3) = NaN;
+
+%% Wheel positions
 
 % i = 1;
 % totalSampleTime = 0;
--- utilities/wheelPosition.m
+++ utilities/wheelPosition.m
@@ -2,15 +2,17 @@
 
 clear; close all; clc;
 
-rad = 1;
+rad = 0.25;
 x0 = 0;
 x1 = 2;
 z0 = 0;
-xCP = 1.4;
+xCP = 2;
 hill = -1;
 flat = 0;
 z1 = hill*4;
-zCP = hill*2.8;
+zCP = hill*4;
+x2 = 3;
+z2 = 3;
 
 if flat == 1
     z1 = z0;
@@ -19,12 +21,15 @@ end
 
 A = [x0, 0, z0];
 B = [x1, 0, z1];
+C = [x2, 0, z2];
 
-u = B-A;
+u = A-B;
+u1 = C-B;
 z_axis = [0, 0, 1];
 x_axis = [1, 0, 0];
 
 alpha = atan2(norm(cross(u,x_axis)),dot(u,x_axis));
+alpha1 = atan2(norm(cross(u1,x_axis)),dot(u1,x_axis));
 
 hW = rad/cos(alpha); % height of the wheel center from the ground
 d = hW*sin(alpha); % distance from the CP of the wheel to the zGround point
@@ -44,6 +49,8 @@ zGround = linearRegression(x0,z0,x1,z1,xW);
 zW = zGround+hW;
 
 plotVectorArrow([x0,z0],[x1,z1])
+hold on
+plotVectorArrow([x1,z1],[x2,z2])
 hold on
 plotVectorArrow([0,0],[1,0])
 hold on
