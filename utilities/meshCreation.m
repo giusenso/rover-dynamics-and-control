f58f6d0ba97b5eedaa08262b2f9392ec1aa52af2
matteofacci <matteofacci.1994@gmail.com>
2021-11-18 19:19:41 +0100

    meshCreation fixed

--- utilities/meshCreation.m
+++ utilities/meshCreation.m
@@ -4,8 +4,10 @@ function terrainProfile = meshCreation(I, mpp, sigma, maxHeight)
 
 figure(1)
 title('Mars surface');
-xLimit = (size(I,2)-1)*mpp;
-yLimit = (size(I,1)-1)*mpp;
+% xLimit = (size(I,2)-1)*mpp;
+% yLimit = (size(I,1)-1)*mpp;
+xLimit = (size(I,2)-1);
+yLimit = (size(I,1)-1);
 RI = imref2d(size(I));
 RI.XWorldLimits = [0 xLimit];
 RI.YWorldLimits = [0 yLimit];
@@ -34,7 +36,7 @@ grayScale = maxHeight*rescale(grayScale);
 
 x = 0:size(grayScale,2)-1;
 y = 0:size(grayScale,1)-1;
-[X,Y] = meshgrid(mpp*x,mpp*y);  % Coordinate Matrices
+[X,Y] = meshgrid(x,y);  % Coordinate Matrices
 
 figure(2)
 title('Mesh Plot')
@@ -57,18 +59,30 @@ surfaceHeight = (max(grayScale,[],'all'));
 % costMap = normalize(double(grayScale), 'range'); % normalize from 0 to 1
 
 % extract pixel values along line
-pixvals = improfile(grayScale,startPose,goalPose);
+pixvals = improfile(grayScale,[startPose(1),goalPose(1)],[startPose(2),goalPose(2)]);
 
-terrainProfile(1,:) = (1:length(pixvals));
+terrainProfile(1,:) = (1:length(pixvals))*mpp;
 terrainProfile(2,:) = pixvals;
 
+terrainProfile = terrainProfile(:,1:end);
+terrainProfile = terrainProfile';
+terrainProfile = sortrows(terrainProfile,1);
+terrainProfile = deleteNaN(terrainProfile);
+terrainProfile = terrainProfile';
+
+shiftFactor = terrainProfile(1,1);
+for i = 1: length(terrainProfile)
+    terrainProfile(1,i)=terrainProfile(1,i)-shiftFactor;
+end
+    
 figure(3)
 title('Terrain Profile')
 RGB_mars = '#934838';
 %plot(1:length(pixvals),pixvals,'k'), axis tight, title('pixel values')
-plot(terrainProfile(1,:),terrainProfile(2,:),'color',RGB_mars);
+plot(terrainProfile(1,:),terrainProfile(2,:),'color',RGB_mars,'Linewidth',2);
 ylabel('Z (m)');
 xlabel('X (m)');
+grid on;
 daspect([1 1 1]);
 %axis tight
 movegui('southeast');
